b25553afa0c0fa45fbb67596cb2aa906d019d997
son-lev-otsv <son.lev@one-line.com>
2025-05-02 17:06:13 +0700

    feat: update

--- router/reviewRouter.js
+++ router/reviewRouter.js
@@ -6,6 +6,7 @@ const auth = require("../middleware/auth");
 router.post("/hotels/:hotelId/reviews", auth, reviewCtrl.addReview);
 router.get("/user/reviews", auth, reviewCtrl.getUserReviews);
 router.put("/reviews/:reviewId", auth, reviewCtrl.updateReview);
+
 router.delete("/reviews/:reviewId", auth, reviewCtrl.deleteReview);
 
 module.exports = router;
